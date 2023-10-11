3f85043f7389bec12ca2a76a142df710940b2496
Michael Dalsbirk Højmose <miho@trackman.com>
2023-10-11 18:32:36 +0200

    initialize project with apple login

--- App.tsx
+++ App.tsx
@@ -1,13 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { AppleAuth } from './components/AppleAuth';
+import { supabase } from './utils/supabase';
+import { useEffect, useState } from 'react';
+import { Session } from '@supabase/supabase-js';
 
 export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    supabase.auth.getSession().then(({data: {session}}) => {
+      setSession(session);
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      {session ? 
+      <>
+        <Button title="sign out" onPress={() => supabase.auth.signOut()} />
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        </>
+      :
+      <AppleAuth />}
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
