92eb2026eb832303db4803e24677e7b1a36aba43
LasseDamgaa <damgaard91@hotmail.com>
2023-10-14 20:16:11 +0200

    [Create_message_page] Install and set up Messages page

--- components/Pages/MessagesPage.tsx
+++ components/Pages/MessagesPage.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat, IMessage } from 'react-native-gifted-chat'
 
-const AboutUsPage = () => {
-    return(
-        <View>
-            <Text>Beskeder</Text>
-        </View>
+const MessagesPage = () => {
+    const [messages, setMessages] = useState<IMessage[]>([])
+    useEffect(() => {
+        // Get messages from supabase database
+        setMessages([
+        {
+            _id: 1,
+            text: 'Hej. Denne besked skal i fremtden hentes fra supabase database',
+            createdAt: new Date(),
+            user: {
+            _id: 2,
+            name: 'Boris',
+            avatar: 'https://picsum.photos/140'
+            },
+        },
+        ])
+    }, [])
+
+    const onSend = useCallback((messages: IMessage[] = []) => {
+        setMessages(previousMessages =>
+            GiftedChat.append(previousMessages, messages),
+        );
+        //Send latest message to supabase-database
+    }, [])
+
+    return (
+        <GiftedChat
+        messages={messages}
+        showAvatarForEveryMessage={true}
+        onSend={messages => onSend(messages)}
+        user={{
+            _id: 1,
+            name: 'user',
+            avatar: 'https://picsum.photos/140'
+        }}
+        />
     )
 }
 
-export default AboutUsPage;
+export default MessagesPage;
