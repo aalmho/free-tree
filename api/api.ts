27d5d6f9a5b9571bbdfa907d8fa1da7c9d71667a
LasseDamgaa <damgaard91@hotmail.com>
2023-10-29 15:57:19 +0100

    [getMessagesFromDb] Problemer med at hente navn på korrespondent via profiles når man henter chats og problem med at opdatere teaser på chats

--- api/api.ts
+++ api/api.ts
@@ -1,6 +1,7 @@
 import { Alert } from "react-native";
 import { supabase, supabaseUrl } from "../utils/supabase";
 import { PostgrestError } from "@supabase/supabase-js";
+import { IMessage, User } from "react-native-gifted-chat";
 
 export type Request = {
   id: number;
@@ -47,6 +48,13 @@ export type Post = {
   requests?: Request[];
 };
 
+export type ChatCardProps = {
+  Id: number,
+  nameOfCorrespondent: string,
+  lastMessageTeaser: string,
+  updated_at: Date
+}
+
 export const uploadImage = async (fileName: string, formData: FormData) => {
   await supabase.storage.from("tree_images").upload(fileName, formData);
 };
@@ -67,11 +75,6 @@ export const createPost = async (
     postal_code: postalCode,
     city,
   });
-  handleError(error);
-};
-
-export const deletePost = async (postId: number) => {
-  const { error } = await supabase.from("posts").delete().eq("id", postId);
   handleError(error);
 };
 
@@ -108,6 +111,95 @@ export const getRequestsByUser = async (userId: string) => {
     .order("created_at", { ascending: false });
   handleError(error);
   return data as RequestMadeByUser[];
+};
+
+export const getFirstName = async (userId: string) => {
+  const { data, error } = await supabase
+    .from("profiles")
+    .select(`id, first_name`)
+    .eq("id", userId)
+  handleError(error);
+  // PROBLEM: data er tom:
+  console.log('namedata', data, userId);
+  if (data && data.length > 0) {
+    return data[0].first_name as string;
+  }
+  else {
+    return ''
+  }
+};
+
+export const getChats = async (userId: string) => {
+  const { data, error } = await supabase
+    .from("chats")
+    .select(
+      `id, updated_at, requester_id, donator_id, is_last_message_read_by_donator, is_last_message_read_by_requester, teaser`
+    )
+    .or(`requester_id.eq.${userId}, donator_id.eq.${userId}`)
+    .order("updated_at", { ascending: false });
+  handleError(error);
+
+  if (!data) {
+    return [];
+  }
+
+  const chatCards: ChatCardProps[] = await Promise.all(data.map(async (chat) => {
+    const correspondentId = chat.requester_id === userId ? chat.donator_id : chat.requester_id;
+    // UDKOMMENTERET FOR NU FORDI NAME ER TOMT 
+    //const nameOfCorrespondent = await getFirstName(correspondentId);
+    const nameOfCorrespondent = 'Michael'
+
+    return {
+      Id: chat.id,
+      nameOfCorrespondent,
+      lastMessageTeaser: chat.teaser,
+      updated_at: chat.updated_at,
+    };
+  }));
+
+  return chatCards as ChatCardProps[];
+};
+
+export const getMessages = async (chatId: number) => {
+  const { data, error } = await supabase
+    .from("messages")
+    .select(
+      `id, author, content, chat_id, created_at`
+    )
+    .eq("chat_id", chatId)
+    .order("created_at", { ascending: false });
+  handleError(error);
+
+  if (!data) {
+    return [];
+  }
+
+  const messages: IMessage[] = await Promise.all(data.map(async (message) => {
+    const author: User = {
+      _id: message.author,
+      name: await getFirstName(message.author)
+    }
+    return {
+      _id: message.id,
+      text: message.content,
+      user: author,
+      createdAt: message.created_at,
+    };
+  }));
+  return messages as IMessage[];
+};
+
+export const sendMessage = async (chatId: number, content: string, author: string ) => {
+  const { error: messageError } = await supabase
+    .from("messages")
+    .insert({ chat_id: chatId, content, author });
+  handleError(messageError);
+  //PROBLEM: UPDATE virker ikke på denne... noget med rettigheder og sikkerhed?
+  const { error: chatError } = await supabase
+  .from("chats")
+  .update({ teaser: content.slice(0, 20) })
+  .eq("id", chatId);
+  handleError(chatError);
 };
 
 export const requestTree = async (requesterUserId: string, postId: number) => {
--- components/chat/MessagesOverview.tsx
+++ components/chat/MessagesOverview.tsx
@@ -1,29 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { TouchableOpacity, View, Text, Image, StyleSheet } from 'react-native';
 import { NavigationProp } from '@react-navigation/native';
-import { peopelIWriteWith } from './MockData';
-import { ChatCardProps } from './types';
+import { useGetChats } from '../../hooks/use-chats';
+import { SessionContext } from '../../context/SessionContext';
+import { ChatCardProps } from '../../api/api';
 
 const MessagesPage = ({ navigation }: { navigation: NavigationProp<any> }) => {
-    const [chatCards, setChatCards] = useState<ChatCardProps[]>([]);
-
-    useEffect( () => {
-        // Fetch messageCards from supaBase. This is mockData.
-        setChatCards(peopelIWriteWith)
-    }, [])
-
+    const { session } = useContext(SessionContext);
+    const { chatCards } = useGetChats(session?.user?.id!)
+    
     const onCardPress = (chatId: number) => {
         navigation.navigate('Chat', {chatId: chatId})
     }
 
     const renderChatCard = (chatCardProps: ChatCardProps) => {
         return(
-            <TouchableOpacity onPress={() => onCardPress(chatCardProps.chatId)} style={styles.chatCard}>
+            <TouchableOpacity onPress={() => onCardPress(chatCardProps.Id)} style={styles.chatCard}>
                 <View style={{flexDirection:'row'}}>
-                    <Image source={{uri: 'https://picsum.photos/140'}} style={{ width: 40, height: 40 }} />
+                    {/* <Image source={{uri: 'https://picsum.photos/140'}} style={{ width: 40, height: 40 }} /> */}
                     <View>
                         <Text>
-                            {chatCardProps.name}
+                            {chatCardProps.nameOfCorrespondent}
                         </Text>
                         <Text>
                             {chatCardProps.lastMessageTeaser}
