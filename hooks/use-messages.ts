f9b2c07f104139a26aacf92ed524947b1b7abb01
LasseDamgaa <damgaard91@hotmail.com>
2023-11-13 23:27:11 +0100

    [messages] realtime

--- hooks/use-messages.ts
+++ hooks/use-messages.ts
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabase";
 import { getMessages, sendMessage } from "../api/api";
 import { IMessage } from "react-native-gifted-chat";
 import { QueryKey, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useGetMessages = (requestId: number) => {
   const [messages, setMessages] = useState<IMessage[]>([]);
+  const getAndSetMessages = () => getMessages(requestId).then((value) => {setMessages(value);});
   useEffect(() => {
-    getMessages(requestId).then((value) => {
-      setMessages(value);
-    });
+    getAndSetMessages()
+    supabase
+      .channel('table-db-changes')
+      .on('postgres_changes', 
+        {
+          event: '*',
+          schema: 'public',
+          table: 'messages'
+        },
+        (payload) => {getAndSetMessages()}
+        ).subscribe();
   }, []);
   return { messages, setMessages };
 };
