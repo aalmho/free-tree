fbcdfd4dba2d8c5433a80f6d26a584d6b5e67a60
Michael Dalsbirk Højmose <miho@trackman.com>
2023-11-23 20:50:36 +0100

    add gesture handler

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { supabase } from "./utils/supabase";
 import { useEffect, useState } from "react";
 import { Session } from "@supabase/supabase-js";
@@ -7,12 +8,11 @@ import "react-native-get-random-values";
 import { SessionContext } from "./context/SessionContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StackNavigator } from "./navigation/StackNavigator";
-import { useTranslation } from "react-i18next";
 import "./i18n/i18next";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   const [session, setSession] = useState<Session | null>(null);
-  const { t } = useTranslation();
 
   const queryClient = new QueryClient();
 
@@ -31,7 +31,7 @@ export default function App() {
   }, []);
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       {session ? (
         <SessionContext.Provider value={{ session }}>
           <NavigationContainer>
@@ -43,6 +43,6 @@ export default function App() {
       ) : (
         <LoginScreen />
       )}
-    </>
+    </GestureHandlerRootView>
   );
 }
